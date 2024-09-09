14f43a741754ede4e49a459c971b797a298c13ce
Miguel Angel Vargas <miguelxtype@gmail.com>
2024-09-09 22:32:28 +0000

    Lab 1

---
+++ Lab_1/hello.cpp
@@ -0,0 +1,11 @@
+// include library of standard input and output commands
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ // Begin main function
+    cout << "Hello world!\n"; // output "Hello World!"
+    return 0; /* indicate successful
+                program completion */
+} // End main function
