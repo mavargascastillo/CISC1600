503148fb4aae802669975858446f29d2654f9365
Miguel Angel Vargas <miguelxtype@gmail.com>
2024-10-15 19:21:28 +0000

    Lab 6

---
+++ Lab_6/randomGame.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+bool randomGame(int randomNumber);
+
+// comments from lab
+// main function is in charge of looping the game
+// third paragraph in the pdf describes what the runGame function should do
